1af3a728f44e95d4d247c6243327d96a45057475
alex-935 <alex.potter3@hotmail.co.uk>
2025-07-29 15:49:09 +0100

    Automated Tests Added for Order Summary

--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,7 +1,7 @@
 import {formatCurrency} from '../../scripts/utils/money.js';
 
 describe('test suite: Format currency', () => {
-    it('converts cents into dollars', () => {
+    it('Converts cents into dollars', () => {
         expect(formatCurrency(2095)).toEqual('20.95');
     });
 
